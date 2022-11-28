d2ed39c2621d7880560bfbe1dc5679ad2c723b81
bryandelonghi <113536458+bryandelonghi@users.noreply.github.com>
2022-11-28 16:08:57 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+/**
+ * -Ask students to say hello in another language
+ * 
+ * -put them up on the board 
+ * 
+ * -celebrate diversity
+ */
 basic.forever(function () {
-    basic.showString("bye")
+    basic.showString("hello")
 })
